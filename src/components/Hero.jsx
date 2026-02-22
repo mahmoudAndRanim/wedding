d2dd8589d003c0c7bb1e8ef2c80330cb38149cb8
Mahmoud Abouchandi <mahmoud.abouchandi@Statnett.no>
2026-02-22 02:40:25 +0100

    Show both Arabic and English names, reduce verse/divider spacing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,9 +30,8 @@ function HeroInner() {
       <p className="invite" data-a="">{t.invite}</p>
 
       <h1 className="names" data-a="">
-        <span>{t.name1}</span>
-        <span className="names-and">{t.and}</span>
-        <span>{t.name2}</span>
+        <span className="names-ar">محمود <span className="names-and-ar">و</span> رنيم</span>
+        <span className="names-en">Mahmoud <span className="names-and-en">&</span> Ranim</span>
       </h1>
 
       <p className="hero-date" data-a="">{t.date}</p>
